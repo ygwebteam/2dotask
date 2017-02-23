7e8cd3f3bff3c4027eb2a747430c64eea0ecc628
Murugan <Murugan@YGSYS-005>
2017-02-23 16:43:22 +0530

    index.html js update Page update

--- app/modules/dashboard/controllers/clients.js
+++ app/modules/dashboard/controllers/clients.js
@@ -1,29 +1,41 @@
-dashboard.controller("ClientsController", ['$rootScope', '$scope', '$state', '$location', 'dashboardService', 'Flash',
-    function ($rootScope, $scope, $state, $location, dashboardService, Flash) {
-        var vm = this;
+﻿/*==========================================================
+    Author      : 2dotask
+    Date Created: 23-02-2017
+    Description : Controller to handle Client page
+    Change Log
+    s.no      date    author     description     
 
-        vm.message = {};
 
-        vm.submitForm = function () {
-            console.log(vm.message);
-            var request = $http({
-                method: "post",
-                url: "",
-                data: vm.message,
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-            });
+ ===========================================================*/
 
-            /* Check whether the HTTP Request is successful or not. */
-            request.success(function (data) {
-                console.log(data);
-                if (data == "success") {
-                    Flash.create('success', 'Message Sent Succesfully', 'large-text');
-                    vm.message = {};
-                    vm.clientForm.$pristine();
-                    vm.clientForm.$setUntouched();
+dashboard.controller("ClientController", ['$rootScope', '$scope', '$state', '$location', 'dashboardService', 'Flash', '$http',
+function ($rootScope, $scope, $state, $location, dashboardService, Flash, $http) {
+    var vm = this;
 
-                }
-            });
-        };
-        console.log("coming to Contact controller");
-    }]);
+    vm.message = {};
+
+    vm.submitForm = function () {
+        console.log(vm.message);
+        var request = $http({
+            method: "post",
+            url: "",
+            data: vm.message,
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+        });
+
+        /* Check whether the HTTP Request is successful or not. */
+        request.success(function (data) {
+            console.log(data);
+            if (data == "success") {
+                Flash.create('success', 'Message Sent Succesfully', 'large-text');
+                vm.message = {};
+                vm.clientForm.$pristine();
+                vm.clientForm.$setUntouched();
+
+            }
+        });
+    };
+    console.log("coming to Contact controller");
+
+}]);
+

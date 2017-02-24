4460e56754d90270d4ad02e9cd4cfb704a9e2c0b
Administrator <Administrator@YGSYS-0013-PC>
2017-02-24 16:54:59 +0530

    added sweet alerts

--- app/modules/dashboard/controllers/clients.js
+++ app/modules/dashboard/controllers/clients.js
@@ -8,34 +8,19 @@
  
  ===========================================================*/
 
-        dashboard.controller("ClientController", ['$rootScope', '$scope', '$state', '$location', 'dashboardService', 'Flash', '$http',
-            function ($rootScope, $scope, $state, $location, dashboardService, Flash, $http) {
-                var vm = this;
-                vm.message = {};
-//                        vm.submitForm = function () {
-//                        var request = $http({
-//                        method: "post",
-//                                url: "/2dotask-angular/2dotask/#/app/clients",
-//                                data:{name : vm.name},
-//                                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-//                        }).success(function () {
-//                        console.log(vm.message);
-//                                $scope.datas = vm.message;
-//                        });
-//                        };
+        dashboard.controller('ClientController', function ($scope, $http) {
+            $scope.errors = [];
+            $scope.msgs = [];
+            $scope.insertData = function (client) {
+                $http.post('/2dotask-angular/2dotask/app/modules/dbhandler/clients.php', {"bname": client.cname})
 
-            }]);
-dashboard.controller('ClientController', function ($scope, $http) {
-$scope.errors = [];
-        $scope.msgs = [];
-        $scope.insertData = function (client) {
-        $http.post('/2dotask-angular/2dotask/app/modules/dbhandler/clients.php', {"bname": client.cname})
-
-                .success(function (data) {
-                if (data === true) {
-                //getInfo();
-                }
-                console.log(data);
-                });
+                        .success(function (data) {
+                            if (data === true) {
+                              
+                            }
+                            console.log(data);
+                        });
+                        swal("Succesfully added Client",'','success');
+                        //$location.reload();
             };
         });

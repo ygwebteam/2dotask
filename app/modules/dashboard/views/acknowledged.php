6bb2f08aada209a6b1313701ee81d369e084e0f8
Murugan <Murugan@YGSYS-005>
2017-03-02 14:26:15 +0530

    task designs

--- app/modules/dashboard/views/acknowledged.php
+++ app/modules/dashboard/views/acknowledged.php
@@ -1,8 +1,34 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+ <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+           Acknowledged Task
+        </h1>
+        <ol class="breadcrumb">
+            <li><a><i class="fa fa-dashboard"></i> Acknowledged Task</a></li>
+            <li class="active">Home</li>
+        </ol>
+    </section>
+   <!-- Main content -->
+<section class="content">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-info" style="min-height:453px;">
+                <div class="panel-heading">
+                    <span>Acknowledged Task</span>
+                   
+                </div>
+                <div class="panel-body" style="overflow: scroll; height: 410px;">
+               <accordion close-others="true">
+                        <accordion-group ng-repeat="currenttasks in currenttask">
+                            <accordion-heading>
+                                <div id="{{currenttasks.id}}">{{currenttasks.project_name}} - {{currenttasks.task_details}}</div><i class="pull-right glyphicon" ng-class="{'glyphicon-chevron-down': isopen, 'glyphicon-chevron-right': !isopen}"></i>
+                            </accordion-heading>
+                            <p ng-repeat="staff in currenttasks.staffs" ><a href="javascript:;" ng-click="showProjectDetails(projects.id)" id="{{projects.id}}" data="{{projects.id}}">{{staff.fname}} {{staff.lname}}</a></p><a href="#addProjects" data-toggle="modal" ng-click="addProject(projects.id);"></a>
+                        </accordion-group>
+                    </accordion>
+           </div>
+            </div>
+        </div>
+     
+    </div>
+</section><!-- /.content -->
